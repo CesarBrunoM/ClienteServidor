2eda199e386de8690bbbb524b27f791483accb90
Bruno C <bcml123@hotmail.com>
2021-09-25 18:03:24 -0300

    Adicionar arquivos de projeto.

---
+++ ServClient/Client.cs
@@ -0,0 +1,82 @@
+﻿using SimpleTcp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServClient
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient client;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Connect();
+                btnSend.Enabled = true;
+                btnConnect.Enabled = true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Mensagem", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (client.IsConnected)
+            {
+                if (!string.IsNullOrEmpty(txtMessage.Text))
+                {
+                    client.Send(txtMessage.Text);
+                    txtInfo.Text+=$"Cliente: { txtMessage.Text}{ Environment.NewLine}";
+                    txtMessage.Text = string.Empty;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new(txtIP.Text);
+            client.Events.Connected += Events_Connected;
+            client.Events.DataReceived += Events_DataReceived;
+            client.Events.Disconnected += Events_Disconnected;
+            btnSend.Enabled = false;
+        }
+
+        private void Events_Disconnected(object sender, ClientDisconnectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"Servidor desconectado.{Environment.NewLine}";
+            });
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"Servidor: {Encoding.UTF8.GetString(e.Data)}{Environment.NewLine}";
+            });
+        }
+
+        private void Events_Connected(object sender, ClientConnectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"Servidor conectado.{Environment.NewLine}";
+            });
+        }
+
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,79 @@
+﻿using SimpleTcp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server;
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            server.Start();
+            txtInfo.Text += $"Iniciado...{Environment.NewLine}";
+            btnStart.Enabled = true;
+            btnSend.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnSend.Enabled = false;
+            server = new SimpleTcpServer(txtIP.Text);
+            server.Events.ClientConnected += Events_ClientConnected;
+            server.Events.ClientConnected += Events_ClientDisconnected;
+            server.Events.DataReceived += Events_DataReceived;
+        }
+
+        private void Events_DataReceived(object sender, DataReceivedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"{e.IpPort}: {Encoding.UTF8.GetString(e.Data)}{Environment.NewLine}";
+            });
+        }
+
+        private void Events_ClientDisconnected(object sender, ClientConnectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"{e.IpPort} disconnected.{Environment.NewLine}";
+                lstClientIP.Items.Remove(e.IpPort);
+            });
+        }
+
+        private void Events_ClientConnected(object sender, ClientConnectedEventArgs e)
+        {
+            this.Invoke((MethodInvoker)delegate
+            {
+                txtInfo.Text += $"{e.IpPort} connected.{Environment.NewLine}";
+                lstClientIP.Items.Add(e.IpPort);
+            });
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (server.IsListening)
+            {
+                if(!string.IsNullOrEmpty(txtMessage.Text) && lstClientIP.SelectedItem != null)
+                {
+                    server.Send(lstClientIP.SelectedItem.ToString(), txtMessage.Text);
+                    txtInfo.Text += $"Servidor: {txtMessage.Text}{Environment.NewLine}";
+                    txtMessage.Text = string.Empty;
+                }
+            }
+        }
+
+    }
+}
